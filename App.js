68ee6f71983a9d7c38360e67f7dbe79c32db860e
Wo1fz <nmy_97@hotmail.com>
2021-08-12 15:15:43 +0800

    creating landing and login page

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { API_KEY } from 'react-native-dotenv'
+
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: 'instagram-clone-9b35f.firebaseapp.com',
+  projectId: 'instagram-clone-9b35f',
+  storageBucket: 'instagram-clone-9b35f.appspot.com',
+  messagingSenderId: '522226968827',
+  appId: '1:522226968827:web:01780eeb9fa19261b4685e',
+  measurementId: 'G-RB21SW8MTF',
+}
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Landing'>
+        <Stack.Screen
+          name='Landing'
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
